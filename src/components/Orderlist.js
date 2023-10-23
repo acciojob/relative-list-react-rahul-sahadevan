0f63e1441463990e6e0086a59af61dd154bd8be1
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-10-23 03:35:50 +0000

    y

--- src/components/Orderlist.js
+++ src/components/Orderlist.js
@@ -3,11 +3,11 @@ import React from "react";
 function Order(){
     return (
         
-        <ol key={'relativeList'}>
-            <li key={'relativeListItem1'}>realtiv1</li>
-            <li key={'relativeListItem2'}>realtiv2</li>
-            <li key={'relativeListItem3'}>realtiv3</li>
+        <ol id="relativeList" key={'relativeList'}>
+            <li id="relativeListItem1" key={'relativeListItem1'}>realtiv1</li>
+            <li id="relativeListItem2" key={'relativeListItem2'}>realtiv2</li>
+            <li id="relativeListItem3" key={'relativeListItem3'}>realtiv3</li>
         </ol>
-    )
+    
 }
 export default Order;
